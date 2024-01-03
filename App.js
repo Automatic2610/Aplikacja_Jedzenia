9f8a5792e0d5f57f812768a806405f6f61ec412a
Automatic2610 <121889060+Automatic2610@users.noreply.github.com>
2024-01-03 10:36:15 +0100

    Aplikacja

---
+++ App.js
@@ -0,0 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from "../Projekt_App_Jedzenie/Pages/Navigation.js";
+
+
+export default function App() {
+  return (
+      <NavigationContainer>
+        <Navigation />
+      </NavigationContainer>
+  );
+}
