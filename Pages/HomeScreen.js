7e8fa526833d09f71b295904950ba3088cc36f1f
Boarsu <133563648+Boarsu@users.noreply.github.com>
2024-01-11 22:44:01 +0100

    HomeScreen

--- Pages/HomeScreen.js
+++ Pages/HomeScreen.js
@@ -1,4 +1,3 @@
-// HomeScreen.js
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Image, StyleSheet, View, Text } from 'react-native';
@@ -8,30 +7,29 @@ import MoreScreen from './MoreScreen';
 
 const Tab = createBottomTabNavigator();
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = () => {
   return (
     <Tab.Navigator
       initialRouteName="MainPage"
-      tabBarOptions={{
-        activeTintColor: '#FF914C',
-        inactiveTintColor: 'gray',
-        labelStyle: { fontSize: 16, fontWeight: 'bold' },
-        style: styles.tabBar,
+      screenOptions={{
+        tabBarActiveTintColor: '#FF914C',
+        tabBarInactiveTintColor: 'gray',
+        tabBarLabelStyle: { fontSize: 16, fontWeight: 'bold' },
+        tabBarStyle: { display: 'flex' },
       }}
     >
-            <Tab.Screen
+      <Tab.Screen
         name="MainPage"
         component={MainPageScreen}
         options={{
-          tabBarLabel: 'Main', // Dodane: Nazwa kafelka
+          tabBarLabel: 'Main',
           tabBarIcon: ({ color }) => (
             <Image
               source={require('../images/logo.png')}
               style={{ tintColor: color, width: 60, height: 50 }}
             />
           ),
-          headerTitle: 'Strona Główna', // Dodane: Nazwa ekranu w nagłówku
-          headerTitleStyle: styles.headerTitle, // Dodane: Styl dla nazwy ekranu w nagłówku
+          headerShown: false,
         }}
       />
       <Tab.Screen
@@ -73,46 +71,58 @@ const HomeScreen = ({ navigation }) => {
           ),
         }}
       />
-      {/* Dodatkowy kafelk z logo i tekstem motywacyjnym */}
     </Tab.Navigator>
   );
 };
 
-// Dodatkowy komponent dla kafelka z logo i tekstem motywacyjnym
 const MainPageScreen = () => (
   <View style={styles.container}>
-    <Image source={require('../images/logo.png')} style={styles.logo} />
-    <Text style={styles.motivationText}>Bądź fit, bądź zdrowy!</Text>
+    <View style={styles.logoContainer}>
+      <Image source={require('../images/logo.png')} style={styles.logo} />
+    </View>
+    <View style={styles.mottoContainer}>
+      <Text style={styles.mottoText}>
+        Zamawiaj pyszne jedzenie i ciesz się smakiem z dostawą pod drzwi!
+      </Text>
+    </View>
+    <View style={styles.deliveryContainer}>
+      <Image source={require('../images/delivery.png')} style={styles.deliveryImage} />
+    </View>
   </View>
 );
 
 const styles = StyleSheet.create({
-  tabBar: {
-    backgroundColor: '#124E78',
-    borderTopColor: 'transparent',
-  },
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#FBFFF1',
   },
-  logo: {
-    width: 150,
-    height: 150,
-    resizeMode: 'contain',
+  logoContainer: {
+    marginTop: 0,
   },
-  motivationText: {
-    fontSize: 20,
+  logo: {
+    width: 350,
+    height: 200,
+  },
+  mottoContainer: {
+    marginTop: 80,
+  },
+  mottoText: {
+    fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
     marginTop: 20,
     color: '#823329',
+    lineHeight: 30, 
   },
-  headerTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#fff', // Kolor nazwy ekranu w nagłówku
+  deliveryContainer: {
+    marginTop: 80,
+  },
+  deliveryImage: {
+    width: 350,
+    height: 150,
+    resizeMode: 'contain',
   },
 });
 
