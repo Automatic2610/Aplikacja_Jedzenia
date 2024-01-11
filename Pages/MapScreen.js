0f01d0ddc554cba7bba3b78ad814e5035ccaf4bd
Boarsu <133563648+Boarsu@users.noreply.github.com>
2024-01-11 16:50:23 +0100

    in progress

---
+++ Pages/MapScreen.js
@@ -0,0 +1,24 @@
+// MapScreen.js
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MapScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>Map Screen</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default MapScreen;
